f8c203cef8d7a908f8af1e83b18040c0e3c103da
stateless-x <purin.buriwong@gmail.com>
2025-08-07 00:42:25 +0700

    remove unused export in Services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,17 +3,4 @@
 
 export { breedService } from './breedService';
 export { petService } from './petService';
-export { userService } from './userService';
-
-// Re-export pagination types for convenience
-export type { PaginationOptions, PaginatedResponse } from '../utils';
-export { 
-  validatePaginationOptions, 
-  calculatePaginationInfo, 
-  createPaginatedResponse,
-  DEFAULT_PAGE_SIZE,
-  MAX_PAGE_SIZE 
-} from '../utils';
-
-// Re-export types from types folder
-export type { BreedFilters, BreedWithDetails, BreedNameResponse } from '../types'; 
+export { userService } from './userService'; 
