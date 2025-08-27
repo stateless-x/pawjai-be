ef08699038f5d57a34f176a6c3050ca2f98a2a27
stateless-x <purin.buriwong@gmail.com>
2025-08-27 17:02:28 +0700

    change drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from 'drizzle-kit';
 import { config  as loadEnv } from 'dotenv';
 
+// Only load .env.local in development
 if (process.env.NODE_ENV !== "production") {
   loadEnv({ path: ".env.local" });
-} else {
-  loadEnv();
 }
+
 export default {
   schema: './src/db/schema.ts',
   out: './src/db/drizzle',
